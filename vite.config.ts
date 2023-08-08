a5ca3a04e4de8b0cb716761ab9a501cf344a8a5d
kamai <kamai122000@mail.ru>
2023-08-09 02:26:27 +0300

    feat: update dep , refactor build

--- vite.config.ts
+++ vite.config.ts
@@ -1,54 +1,31 @@
-import typescript from '@rollup/plugin-typescript'
-import path from 'path'
-import colors from 'picocolors'
-import { typescriptPaths } from 'rollup-plugin-typescript-paths'
-import { visualizer } from 'rollup-plugin-visualizer'
 import { defineConfig } from 'vite'
-import progress from 'vite-plugin-progress'
+import dts from 'vite-plugin-dts'
+import path from 'path'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
 	plugins: [
-		progress({
-			format: `Building ${colors.green('[:bar]')} :percent :eta`,
-			total: 100,
-			width: 60,
-			// complete: "=",
-			// incomplete: "",
+		react(),
+		dts({
+			insertTypesEntry: true,
 		}),
 	],
-
-	server: {
-		port: 3123,
-	},
-	// preview: {
-	//   port: 3124,
-	// },
 	build: {
-		manifest: true,
-		minify: true,
-		reportCompressedSize: true,
 		lib: {
-			entry: path.resolve(__dirname, 'src/main.ts'),
-			fileName: 'main',
-			formats: ['es', 'cjs'],
+			entry: path.resolve(__dirname, 'src/index.ts'),
+			name: 'ReactTSLib',
+			formats: ['es', 'umd'],
+			fileName: (format) => `react-ts-lib.${format}.js`,
 		},
 		rollupOptions: {
-			external: [],
-			plugins: [
-				typescriptPaths({
-					preserveExtensions: true,
-				}),
-				typescript({
-					declaration: true,
-					sourceMap: true,
-					outDir: 'dist',
-					exclude: ['**/__tests__'],
-				}),
-				visualizer({
-					title: 'visualizer - vite-vanilla-ts-module',
-					template: 'network',
-				}),
-			],
+			external: ['react', 'react-dom', 'styled-components'],
+			output: {
+				globals: {
+					react: 'React',
+					'react-dom': 'ReactDOM',
+					'styled-components': 'styled',
+				},
+			},
 		},
 	},
 })
